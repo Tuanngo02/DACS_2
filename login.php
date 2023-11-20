400762b25730784867b3ac440743d1a0436d13ad
Tuanngo02 <thanhtuanhht0211@gmail.com>
2023-11-20 20:31:05 +0700

    first

---
+++ login.php
@@ -0,0 +1,65 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Bụi Camp- Đăng nhập</title>
+<script src="js/jquery.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css1/stylelogin.css" rel="stylesheet" type="text/css" media="all"/>
+<!-- for-mobile-apps -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<meta name="keywords" content="Classy Login form Responsive, Login form web template, Sign up Web Templates, Flat Web Templates, Login signup Responsive web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<!-- //for-mobile-apps -->
+<!--Google Fonts-->
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<!--header start here-->
+<div class="header">
+	
+		<div class="header-main">
+			<a href="index.php">
+			<img src="favicon.jpg" style="width: 150px; border-radius: 50%; margin-left:155px; margin-top:-70px;" alt="">
+		    </a>   
+			<h1>Đăng nhập</h1>
+			<div class="header-bottom">
+				<div class="header-right w3agile">
+					
+					<div class="header-left-bottom agileinfo">
+						
+					 <form action="#" method="post">
+						<input type="text"  value="Tên đăng nhập" name="name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'User name';}"/>
+					<input type="password"  value="Password" name="password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'password';}"/>
+						<div class="remember">
+			             <span class="checkbox1">
+						 <h6><a href="#" style="color: white;">Quên mật khẩu?</a></h6>
+						 </span>
+						 <div class="forgot">
+						 	<h6><a href="register.php">Bạn chưa có tài khoản?</a></h6>
+						 </div>
+						<div class="clear"> </div>
+					  </div>
+					   
+						<input type="submit" value="Đăng nhập">
+					</form>	
+					
+						
+				</div>
+				</div>
+			  
+			</div>
+		</div>
+</div>
+<!--header end here-->
+<div class="copyright">
+	<p>© 2016 Classy Login Form. All rights reserved | Design by  <a href="http://w3layouts.com/" target="_blank">  W3layouts </a></p>
+</div>
+<!--footer end here-->
+</body>
+</html>
