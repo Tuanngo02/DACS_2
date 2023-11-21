9306f8e60dad419a24faaee65e6e478fcbcdbd75
longthien123 <longthienl80@gmail.com>
2023-11-21 21:07:27 +0700

    database

--- CSDL/camping.sql
+++ CSDL/camping.sql
@@ -1,94 +1,348 @@
-CREATE TABLE `Role` (
-  `id` int PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  `name` varchar(20)
-);
-use camping;
-CREATE TABLE `Users` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `role_id` int,
-  `username` varchar(100),
-  `password` varchar(100),
-  `fullname` varchar(50),
-  `email` varchar(150),
-  `phone` varchar(20),
-  `address` varchar(200),
-  `birthday` int(11) NOT NULL,
-  `created_time` int(11) NOT NULL,
-  `last_updated` int(11) NOT NULL
-);
--- INSERT INTO `Users` (`id`, `username`, `fullname`, `password`, `birthday`, `created_time`, `last_updated`) VALUES
--- (1, 'admin', 'admin', '123', 1553185530, 1553185530, 1553185530),
--- (3, 'writer', 'Tuấn', '123', 1553185530, 1553185530, 1553185530);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2023 at 03:05 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
-CREATE TABLE `Category` (
-  `id` int PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  `name` varchar(100)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `camping`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Lều'),
+(2, 'Vật dụng'),
+(3, 'Thức ăn'),
+(4, 'Đồ uống'),
+(5, 'Nội thất');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `note` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_library`
+--
+
 CREATE TABLE `image_library` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `path` varchar(255) NOT NULL,
+  `thumbails` varchar(255) NOT NULL,
   `created_time` int(11) NOT NULL,
   `last_updated` int(11) NOT NULL
-);
-CREATE TABLE `Products` (
-  `id` int PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  `category_id` int,
-  `name` varchar(350),
-  `image` varchar(255) DEFAULT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `image_library`
+--
+
+INSERT INTO `image_library` (`id`, `product_id`, `thumbails`, `created_time`, `last_updated`) VALUES
+(1, 1, '1.jpg', 519211123, 519211123),
+(2, 1, '2.jpg', 1205211123, 1205211123),
+(3, 1, '3.jpg', 21312321, 12312312),
+(4, 1, '4.jpg', 1205211123, 1205211123),
+(5, 2, '1.jpg', 21312321, 12312312),
+(6, 1, '5.png', 1205211123, 1205211123),
+(7, 2, '2.png', 21312321, 12312312);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oders`
+--
+
+CREATE TABLE `oders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `fullname` varchar(50) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `note` varchar(500) DEFAULT NULL,
+  `order_date` datetime DEFAULT NULL,
+  `status` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `oder_details`
+--
+
+CREATE TABLE `oder_details` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `num` int(11) DEFAULT NULL,
+  `total_money` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `name` varchar(350) DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
   `price` float NOT NULL,
   `content` text NOT NULL,
   `created_time` int(11) NOT NULL,
   `last_updated` int(11) NOT NULL
-);
--- INSERT INTO `Products` (`id`, `name`, `image`, `price`, `content`, `created_time`, `last_updated`) VALUES
--- (2, 'GiÃ y bÃ³ng Ä‘Ã¡ 1', 'uploads/06-03-2019/shoes-1.jpg', 540000, 'GiÃ y bÃ³ng Ä‘Ã¡ 1 sÃ¢n cá» nhÃ¢n táº¡o', 1552615987, 1552615987);
-CREATE TABLE `Galery` (
-  `id` int PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  `product_id` int,
-  `thumbnail` varchar(500)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `Comment` (
-  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `user_id` int,
-  `product_id` int,
-  `note` varchar(500)
-);
+--
+-- Dumping data for table `products`
+--
 
-CREATE TABLE `Oders` (
-  `id` int PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  `user_id` int,
-  `fullname` varchar(50),
-  `email` varchar(150),
-  `phone` varchar(20),
-  `address` varchar(200),
-  `note` varchar(500),
-  `order_date` datetime,
-  `status` int
-);
+INSERT INTO `products` (`id`, `category_id`, `name`, `image_path`, `price`, `content`, `created_time`, `last_updated`) VALUES
+(1, 1, 'Lều 2 người Kelty Salida mới', '1', 850000, 'Chất Liệu: vải 190t PU 2000 .Lưới Lỗ 68.\nKích Thước: Dài 2.0m, Rộng 1.4 m, Cao 1.3m.\nCân nặng: 2.0Kg, đựng trong túi 60cmx20xm20cm\nKhung Lều: Khung sợi thủy tinh 8.5mm .veron inox , cọc thép 5.5mm 9 cây.\nMô Tả: Lớp trong lưới 68 ,lều có 1 cửa chính,cửa có 2 lớp .Lớp Vải Ngoài 190t chống thấm tuyệt đối, có tăng đưa tăng giảm .', 1205211123, 1205211123),
+(2, 1, 'Lều Vintage 2 Người mẫu 2023', '2', 1200000, 'LỀU VINTAGE HOME 2- 3P\r\n2m10 x 1m60 x cao 140 .\r\nTùy biến. 2 Cửa. 2 Lớp.\r\nVải chính: TC 65/35 .COTTON. CANVAS. RA/BEIGE .\r\nVải đáy: P/O 210DPU3000M/MWRFR.\r\nĐựng trong túi xách 60 x 20 .\r\nTrọng lượng trọn bộ: 5kg', 1224211123, 1224211123);
 
-CREATE TABLE `Oder_details` (
-  `id` int PRIMARY KEY  NOT NULL AUTO_INCREMENT,
-  `order_id` int,
-  `product_id` int,
-  `price` int,
-  `num` int,
-  `total_money` int
-);
+-- --------------------------------------------------------
 
-ALTER TABLE `Users` ADD FOREIGN KEY (`role_id`) REFERENCES `Role` (`id`);
+--
+-- Table structure for table `role`
+--
 
-ALTER TABLE `Comment` ADD FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`);
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Comment` ADD FOREIGN KEY (`product_id`) REFERENCES `Products` (`id`);
+--
+-- Dumping data for table `role`
+--
 
-ALTER TABLE `Products` ADD FOREIGN KEY (`category_id`) REFERENCES `Category` (`id`);
+INSERT INTO `role` (`id`, `name`) VALUES
+(1, 'admin'),
+(2, 'custom');
 
-ALTER TABLE `Oder_details` ADD FOREIGN KEY (`order_id`) REFERENCES `Oders` (`id`);
+-- --------------------------------------------------------
 
-ALTER TABLE `Galery` ADD FOREIGN KEY (`product_id`) REFERENCES `Products` (`id`);
+--
+-- Table structure for table `users`
+--
 
-ALTER TABLE `Oder_details` ADD FOREIGN KEY (`product_id`) REFERENCES `Products` (`id`);
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `fullname` varchar(50) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `birthday` int(11) NOT NULL,
+  `created_time` int(11) NOT NULL,
+  `last_updated` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Oders` ADD FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`);
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `role_id`, `username`, `password`, `fullname`, `email`, `phone`, `address`, `birthday`, `created_time`, `last_updated`) VALUES
+(1, 1, 'admin', '123', 'admin', 'admin@gmail.com', '0123456789', 'Da Nang', 2004, 20112023, 20112023),
+(4, 2, 'longthien', '123', 'Lê Long Thiên', 'longthienl80@gmail.com', '0787626825', 'Hòa Vang', 0, 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `image_library`
+--
+ALTER TABLE `image_library`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `oders`
+--
+ALTER TABLE `oders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `oder_details`
+--
+ALTER TABLE `oder_details`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `image_library`
+--
+ALTER TABLE `image_library`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `oders`
+--
+ALTER TABLE `oders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `oder_details`
+--
+ALTER TABLE `oder_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `image_library`
+--
+ALTER TABLE `image_library`
+  ADD CONSTRAINT `image_library_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `oders`
+--
+ALTER TABLE `oders`
+  ADD CONSTRAINT `oders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `oder_details`
+--
+ALTER TABLE `oder_details`
+  ADD CONSTRAINT `oder_details_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `oders` (`id`),
+  ADD CONSTRAINT `oder_details_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
